6b16d423e4a59de6b631bd58c8ef0bd6379b78d4
unknown <ffarras00@gmail.com>
2020-10-19 11:06:29 +0700

    hapus file template

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Azura Labs</title>
-    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="/atlantis/css/bootstrap.min.css">
     <link rel="stylesheet" href="/atlantis/css/atlantis.min.css">
@@ -15,15 +16,20 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
     <script src="atlantis/js/plugin/webfont/webfont.min.js"></script>
-	<script>
-		WebFont.load({
-			google: {"families":["Lato:300,400,700,900"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['atlantis/css/fonts.min.css']},
-			active: function() {
-				sessionStorage.fonts = true;
-			}
-		});
-	</script>
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Lato:300,400,700,900"]
+            },
+            custom: {
+                "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"],
+                urls: ['atlantis/css/fonts.min.css']
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
 
     <style>
         .parsley-error {
@@ -33,6 +39,7 @@
 
     @yield('styles')
 </head>
+
 <body>
     <div class="wrapper">
         @include('_layouts.app.header')
@@ -50,9 +57,10 @@
     </div>
 
     <script src="{{ mix('js/app.js') }}"></script>
-	<script src="{{ mix('js/plugin.js') }}"></script>
+    <script src="{{ mix('js/plugin.js') }}"></script>
     <!-- <script src="{{ mix('js/setting-demo.js') }}"></script> -->
-    <script src="atlantis/js/demo.js"></script>
+    <!-- <script src="atlantis/js/demo.js"></script> -->
     @stack('scripts')
 </body>
+
 </html>
--- resources/views/_layouts/auth.blade.php
+++ resources/views/_layouts/auth.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Azura Labs</title>
-    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="atlantis/css/bootstrap.min.css">
     <link rel="stylesheet" href="atlantis/css/atlantis.min.css">
     <link rel="stylesheet" href="atlantis/css/fonts.css">
     @stack('styles')
 </head>
+
 <body>
     <div class="wrapper overlay-sidebar">
         @include('_layouts.auth.header')
@@ -29,15 +31,9 @@
     </div>
 
     <script src="atlantis/js/app.js"></script>
-	<script src="atlantis/js/plugin.js"></script>
-    <script src="atlantis/js/setting-demo.js"></script>
-    <!-- <script src="atlantis/js/core/jquery.3.2.1.min.js"></script> -->
-	<script src="atlantis/js/core/popper.min.js"></script>
-	<script src="atlantis/js/core/bootstrap.min.js"></script>
-	<!-- jQuery UI -->
-	<script src="atlantis/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<script src="atlantis/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+    <script src="atlantis/js/plugin.js"></script>
 
     @stack('scripts')
 </body>
+
 </html>
